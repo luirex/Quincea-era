4672e2c0591cbdc5cc9faefb131512daaa244a97
luirex <luiggy31@gmail.com>
2026-09-04 14:51:51 -0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+/* Configuración de Firebase - Cabina de Fotos XV */
+window.CABINA_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDsydCWIJKYq4BDtmkzay3IzMxPud2MxPQ",
+  authDomain: "cabina-fotos-xv.firebaseapp.com",
+  projectId: "cabina-fotos-xv",
+  storageBucket: "cabina-fotos-xv.firebasestorage.app",
+  messagingSenderId: "721125705010",
+  appId: "1:721125705010:web:df56b431937dfea2ea832b"
+};
+
+window.CABINA_ADMIN_EMAIL = "luiggy31@gmail.com";
